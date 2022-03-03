8603b0250f3f8cde31913f1e19d4fd6448369b7f
Ashour100 <interinstitute5@gmail.com>
2022-03-03 12:50:00 +0100

    JS Snack 3 update

--- JS Snack 3 (3 custom)/main.js
+++ JS Snack 3 (3 custom)/main.js
@@ -1,40 +1,67 @@
-/**
- *
- *
-    Crea due tag div con due id diversi:
-    un div avrà il testo colorato di rosso mentre l'altro di verde.
-    Partendo da un array di numeri, creo una funzione che controlla se siano pari o dispari.
-    Se sono pari li stampo nell'div "rosso",
-    se sono dispari li stampo nell'div "verde".
- *
- */
+// /**
+//  *
+//  *
+//     Crea due tag div con due id diversi:
+//     un div avrà il testo colorato di rosso mentre l'altro di verde.
+//     Partendo da un array di numeri, creo una funzione che controlla se siano pari o dispari.
+//     Se sono pari li stampo nell'div "rosso",
+//     se sono dispari li stampo nell'div "verde".
+//  *
+//  */
 
 
-function isPari(num){
-    if(num%2==0)
+// function isPari(num){
+//     if(num%2==0)
+//         return true;
+
+//     return false;
+// }
+// v=parseInt(prompt("Dim arr"));
+// arr=[v];
+// let numero;
+// for(let i=0;i<v;i++){
+//     numero=parseInt(prompt("Inserisci un numero"));
+//     if(isPari(numero))
+//         document.getElementById("first").innerHTML+="<br>"+numero;
+//     else
+//         document.getElementById("second").innerHTML+="<br>"+numero;
+// }
+
+
+
+// let Dim= document.getElementById("Dim");
+
+
+// if(isPari(v))
+//     Dim.classList.add("text-danger");
+// else
+//     Dim.classList.add("text-success");
+
+// Dim.innerHTML="<br> Dim="+v;
+
+
+
+
+
+// * creo un'altra funzione che controlla che la somma di due numeri x e y sia pari
+// *
+// * sommaPari(24214, 12451241) ==> true se la somma mi dà un numero pari
+// *                            ==> false se la somma mi dà un numero dispari
+// */
+
+
+let num1=parseInt(prompt("insert a number"));
+let num2=parseInt(prompt("insert a number"));
+
+function sommaPari(num1,num2){
+    let somma=num1+num2;
+    if(somma%2==0)
         return true;
-
+    
     return false;
 }
-v=parseInt(prompt("Dim arr"));
-arr=[v];
-let numero;
-for(let i=0;i<v;i++){
-    numero=parseInt(prompt("Inserisci un numero"));
-    if(isPari(numero))
-        document.getElementById("first").innerHTML+=numero;
-    else
-        document.getElementById("second").innerHTML+=numero;
-}
 
-
-
-let Dim= document.getElementById("Dim");
-
-
-if(isPari(v))
-    Dim.classList.add("text-danger");
+if(sommaPari(num1,num2))
+    alert("la somma è pari");
 else
-    Dim.classList.add("text-success");
-
-Dim.innerHTML="Dim="+v;
+    alert("la somma è dispari");   
